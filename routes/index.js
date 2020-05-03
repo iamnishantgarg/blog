487d778b9a3bb4d86e19ebb8bdf527534eb2d238
Nishant garg <iamnishantgarg@gmail.com>
2020-05-04 01:07:00 +0530

    add post shifted to dashboard

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const { ensureAuthenticated } = require("../config/checkauth");
 const indexController = require("../controller/index");
 const router = express.Router();
 router.get("/", ensureAuthenticated, indexController.getIndex);
-router.get("/add-post", ensureAuthenticated, indexController.getAddPost);
+// router.get("/add-post", ensureAuthenticated, indexController.getAddPost);
 router.post("/add-post", indexController.postAddPost);
 router.post("/delete-post", indexController.postDeletePost);
 router.post("/post-comment", indexController.postPostComment);
